3ab5332c06c1841ea540c50d2dd993b5d0759604
Wachira007 <dannielmutua00@gmail.com>
2022-09-28 07:46:44 -0700

    Python input and output

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines string-to-JSON function."""
+import json
+
+def to_json_string(my_obj):
+    """Return JSON representation of string object."""
+    return json.dumps(my_obj)
